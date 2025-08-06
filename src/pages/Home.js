4fee7ebf26f869db931064aba0ad08eaf5488ab0
Kashika2004s <kashikassdn28@gmail.com>
2025-08-06 13:00:33 +0530

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,15 +43,7 @@ const Home = () => {
 					alt="code-sync-logo"
 				/>
 				<h4 className="mainLabel">Paste invitation ROOM ID</h4>
-				<div className="inputGroup">
-					<input
-						type="text"
-						className="inputBox"
-						placeholder="ROOM ID"
-						onChange={e => setRoomId(e.target.value)}
-						value={roomId}
-						onKeyUp={handleInputEnter}
-					/>
+				
 					<input
 						type="text"
 						className="inputBox"
@@ -63,16 +55,7 @@ const Home = () => {
 					<button className="btn joinBtn" onClick={joinRoom}>
 						Join
 					</button>
-					<span className="createInfo">
-						If you don't have an invite then create &nbsp;
-						<a
-							onClick={createNewRoom}
-							href=""
-							className="createNewBtn"
-						>
-							new room
-						</a>
-					</span>
+					
 				</div>
 			</div>
 		</div>
